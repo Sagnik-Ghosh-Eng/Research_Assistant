e5eb3cdffc86ba93453adba4a5b82a607010a1f2
Sagnik Ghosh <sagnikofficial91827@gmail.com>
2026-01-29 20:53:15 +0530

    Initial commit: Project folders and files

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+import os
+
+GEMINI_API_KEY = "AIzaSyCCZmcgR6O0T8RzlVdArtPADvOPvrSmd28"
+
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+CHUNK_SIZE = 800
+CHUNK_OVERLAP = 100
+TOP_K = 5
+
+
+
+
+
+
